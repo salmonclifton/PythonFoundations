6dd58affb7ca86814c05ed5346c160fbf00cb06f
David Clifton <cliftondr@gmail.com>
2020-01-08 07:48:16 -0800

    Small text update

--- Mod1_Lab3.py
+++ Mod1_Lab3.py
@@ -4,7 +4,7 @@ user_name = input("What is your name? ")
 user_color = input("What is your favorite color? ")
 #Output name,  favorite color and a Monty Python witicism
 print("Hi " + user_name + "!")
-print("I hear your favorite color is " + user_color + "."\
-"\nMy name is David and my favorite color is green.")\
+print("I hear that your favorite color is " + user_color + "."\
+"\nMy name is David and my favorite color is green."\
 "\nWhat is the maximum airspeed of an unladen swallow?")
 
