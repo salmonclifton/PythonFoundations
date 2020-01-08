d71edef7e2ccf505cf6413c660f0e15ae3808e68
David Clifton <cliftondr@gmail.com>
2020-01-07 22:54:44 -0800

    Initial

---
+++ hw1_function.py
@@ -0,0 +1,12 @@
+#Prompt the user with question 1 and store in the variable
+answer1 = input("Question1?")
+#Prompt the user with question 2 and store in the variable
+answer2 = input("Question2?")
+#Prompt the user with question 3 and store in the variable
+answer3 = input("Question3?")
+#Create a function to create and output the sentence
+def sentence_maker(p1,p2,p3):
+    print(answer1 + answer2 + answer3)
+
+sentence_maker(answer1, answer2, answer3)
+
