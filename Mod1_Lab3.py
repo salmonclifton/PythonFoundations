e23bb32a57969bb61c803b200ac10953fbf72fb3
David Clifton <cliftondr@gmail.com>
2020-01-08 07:38:52 -0800

    Reduced the print lines from three to one.

---
+++ Mod1_Lab3.py
@@ -0,0 +1,10 @@
+#Prompt user for their name and store as a variable
+user_name = input("What is your name? ")
+#Prompt user for their favorite color and store as a variable
+user_color = input("What is your favorite color? ")
+#Output name,  favorite color and a Monty Python witicism
+print("Hi " + user_name + "!")
+print("I hear your favorite color is " + user_color + "."\
+"\nMy name is David and my favorite color is green.")\
+"\nWhat is the maximum airspeed of an unladen swallow?")
+
