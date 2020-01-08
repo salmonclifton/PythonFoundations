8d828d10004d056468ca368cb671d51a06a7ad02
David Clifton <cliftondr@gmail.com>
2020-01-07 22:52:52 -0800

    Initial

---
+++ hw1.py
@@ -0,0 +1,10 @@
+#Prompt the user with question 1 and store in the variable
+answer1 = input("Question1?")
+#Prompt the user with question 2 and store in the variable
+answer2 = input("Question2?")
+#Prompt the user with question 3 and store in the variable
+answer3 = input("Question3?")
+#Combine the inputs into a sentence
+
+#Output the sentence
+
