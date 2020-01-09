345f75f57dbb313359f4722a2f80dff00e2e12cc
David Clifton <cliftondr@gmail.com>
2020-01-09 15:42:20 -0800

    Full solution with calling a function and format string

--- hw1.py
+++ hw1.py
@@ -1,10 +1,24 @@
-#Prompt the user with question 1 and store in the variable
-answer1 = input("Question1?")
-#Prompt the user with question 2 and store in the variable
-answer2 = input("Question2?")
-#Prompt the user with question 3 and store in the variable
-answer3 = input("Question3?")
-#Combine the inputs into a sentence
+"""
+This solution prompts the user with three questions and stores
+each answer in a variable.
+A function is called passing it the three variables and the function
+outputs the three responses in a sentence using string format.
+"""
+#Prompt the user with question 1 and store in the variable.
+feline = input("What is your favorite feline? ")
 
-#Output the sentence
+#Prompt the user with question 2 and store in the variable.
+canine = input("What is your favorite canine? ")
+
+#Prompt the user with question 3 and store in the variable.
+weather = input("What is your favorite weather condition? ")
+
+#Create a function to form and output the sentence using the
+# three parameters passed to it.
+def sentence_maker(p1,p2,p3):
+    print("\nTomorrow it will be raining {}s and {}s with "\
+          "intermittent periods of {}.".format(p1, p2, p3))
+
+#Call the function passing it the three user inputs as parameters.
+sentence_maker(feline, canine, weather)
 
