7123e7de4e8810a7e199b2348ef94bf6f165a674
David Clifton <cliftondr@gmail.com>
2020-01-25 17:59:19 -0800

    Working with cleaning input and list comprehension

--- Mod3XtraCredit.py
+++ Mod3XtraCredit.py
@@ -49,25 +49,60 @@ max_guesses = length + 3
 
 # use a while loop with a sentry variable
 index = 0
-while index <= length:
+valid = "no"
+repeat = "yes"
+correct_guesses = []
+incorrect_guesses = []
+
+while index < max_guesses:
+    # get user input of a letter
+    while valid == "no" or repeat == "yes":
+        guess = input("What letter do you want to guess? ")
+        #guess = input("What letter do you want to guess? ")
+        guesses = incorrect_guesses + correct_guesses
+        guesses.sort()
+        # validate that the user is not repeating a guess
+        if guess in guesses:
+            print("\nPrevious guesses: ", ' '.join('{}'.format(*i) for i in (guesses)))
+            print("Please enter a letter that you have not guessed before.")
+        else:
+            repeat = "no"
+        #validate that the user is entering a single letter
+        if (not guess.isalpha()) or (len(str(guess))) != 1:
+            print("Please guess a single letter.")
+        else:
+            valid = "yes"
+
+    valid = "no"
+    repeat = "yes"
+
+    # check if the letter is in the animal name
+    # store the guessed letter in a list
+    if guess in animal:
+        correct_guesses.append(guess)
+        print("Correct: ", correct_guesses)
+    else:
+        incorrect_guesses.append(guess)
+        print("Inorrect: ", incorrect_guesses)
     # increment the sentry variable
     index += 1
 
-    # get user input of a letter
-    guess = input("What letter do you guess? ")
-
-    # check if the letter is in the animal name
-    if animal.index(guess):
-        for index, letter in enumerate(animal):
-            print("yes!")
-            check = animal.index(guess)
-            print(check)
-    # store the guessed letter in a list
-
-    # increment the sentry variable
-
     # print out the status of the game using a for loop (go though the letters in the animal name and see if they're in the guess letter list
+    status =[]
+    for letter in animal:
+        if letter in correct_guesses:
+            #print("Yes!")
+            status.append(letter)
+        else:
+            #print("No!")
+            status.append("-")
+    print(' '.join('{}'.format(*i) for i in (status)))
 
     # if all of the letters in the animal are in the guess letter list, break and congratulate the user
-
+    if "-" not in status:
+        print("You win!")
+        break
     # if the sentry variable is bigger or equal to the guesses, break and console the loser
+    if index > length:
+        print("You lose!")
+        break
