c3e1d6f0dc119414d970167a59435d9a3310ec40
David Clifton <cliftondr@gmail.com>
2020-02-08 20:56:52 -0800

    Base homework solution

--- Mod5XtraCredit.py
+++ Mod5XtraCredit.py
@@ -13,5 +13,20 @@ Write the dictionary to a file
 import csv
 fh = open("./City_of_Seattle_Wage_Data.csv")
 reader = csv.reader(fh)
-for row in reader:
-    print(row)
+
+#Store the header line in a variable using next
+header = next(reader)
+
+#Create a dictionary to store the list of 'Hourly Rate' by job title (hint: this is a dictionary where the value is
+# a list - seattle[job] = [rates].  You'll need to use key in dictionary or dictionary.get to see if the key exists in
+# each dictionary and create it if it does not, similar to counting with dictionaries.)
+
+from collections import defaultdict
+job_rates = defaultdict(list)
+for dept, last, first, key, rate in reader:
+    job_rates[key].append(rate)
+print(job_rates)
+
+w = csv.writer(open("job_rates.csv", "w"))
+for key, val in job_rates.items():
+    w.writerow([key, val])
