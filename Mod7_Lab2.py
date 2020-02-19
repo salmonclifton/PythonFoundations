9faa4545796a25e9b1960273edd56301e05e654b
David Clifton <cliftondr@gmail.com>
2020-02-19 15:33:08 -0800

    Submitted working version

--- Mod7_Lab2.py
+++ Mod7_Lab2.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
+import csv
 import requests
 import lxml
 from bs4 import BeautifulSoup
 
-url = "https://en.wikipedia.org/wiki/List_of_craters_on_the_Moon (Links to an external site.)"
+url = "https://en.wikipedia.org/wiki/List_of_craters_on_the_Moon"
+
 # use requests to get the url
+response = requests.get(url)
+
 # get the content from the response
+content = response.content
 
 # parse the content with soup
 soup = BeautifulSoup(content, 'lxml')
@@ -14,22 +19,43 @@ soup = BeautifulSoup(content, 'lxml')
 # look at the web page source and find out the container for the information we want
 # each crater is stored in a li that has the class 'gallerybox'
 # tell soup to get the li gallerybox
+gb_lines = soup.find_all("li", "gallerybox")
 
 # crater name and crater diameter are inside a div that has the class 'gallerytext'
-# tell soup to get the div gallerytext from div gallerybox
+# tell soup to get the div gallerytext from li gallerybox
+gt_lines = soup.find_all("div", "gallerytext")
 
-# crater name the string of an anchor tag that is inside a div that has a class 'gallerytext'
-# tell soup to get the anchor tag from div gallerytext
+#Create dictionary by populating headers
+crater_dict = {"Crater":"Diameter"}
 
-# crater diameter is the string of a span tag that is inside a div that has class 'gallerytext'
-# tell soup to get the span tag from div gallerytext
+#For loop to get crater name from anchor tag and diameter from span tag within div galerytext and
+#add each pair to the dictionary
+for line in gb_lines:
+    div_tag = line.find("div", "gallerytext")
+    if div_tag is not None:
+        # crater name the string of an anchor tag that is inside a div that has a class 'gallerytext'
+        # tell soup to get the anchor tag from div gallerytext
+        crater = div_tag.find("a")
+        crater = crater.string
 
-# crater thumbnail is inside of the div that has the class 'thumb'
-# tell soup to get the div thumb from div gallerybox
+        # crater diameter is the string of a span tag that is inside a div that has class 'gallerytext'
+        # tell soup to get the span tag from div gallerytext
+        diameter = div_tag.find("span")
+        diameter = diameter.string
+        diameter = diameter.replace('(', '').replace(')', '')
 
-# crater thumbnail source is the 'src' key inside the 'attrs' dictionary of the img tag from div thumb
-# tell soup to get the img tag from div thumb
-
-#store name = diameter or name = thumbnail in a dictionary
+        # store name = diameter in a dictionary
+        crater_dict.update({crater: diameter})
 
 # print dictionary key/value to a file as two columns separated by a comma
+with open("crater_csv.csv", "w") as outfile:
+    for crater, diameter in crater_dict.items():
+        crater = crater.replace(':', '')
+        outfile.writelines("{}, {}".format(crater, diameter))
+        outfile.write("\n")
+
+# Open newly created csv file and print
+fh = open("./crater_csv.csv")
+reader = csv.reader(fh)
+for row in reader:
+    print(row)
